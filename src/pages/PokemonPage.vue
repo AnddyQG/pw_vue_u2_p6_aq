80f43e3aa743ce60c3d42ad4de5bef7f77adbc30
AnddyQG <anddysk819@gmail.com>
2024-06-25 22:19:26 -0500

    tarea 4

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -2,6 +2,8 @@
     <h1 v-if="!pokemonCorrecto">Porfavooor espere.................</h1>
     <div v-else>
         <h1>Selecciona el pokemon correcto:</h1>
+        <h2> Puntuacion : {{ puntuacion }}</h2>
+        <h2>Intento : {{ intento }}</h2>
         <PokemonImagen :idPokemon="pokemonCorrecto.id" :mostrarPokemon="mostrar"/>
         <PokemonOpciones :pokemons="arreglo"@seleccionPokemon="reviarRespuesta($event)"/>
 
@@ -24,26 +26,53 @@ export default {
     },
     methods:{
         async cargaInicial(){
-            const vectorInicial= await obtenerPokemonsFachada(4);
+            const vectorInicial= await obtenerPokemonsFachada(7);
             this.arreglo= vectorInicial;
 
             const indice= Math.floor(Math.random()*7);
             this.pokemonCorrecto= this.arreglo[indice];
         },
         reviarRespuesta(dato){
-            console.log('Se emitio un evento desde el niño')
-            console.log(dato);
+    console.log('Se emitio un evento desde el niño')
+    console.log(dato);
 
-            if(dato.ident===this.pokemonCorrecto.id){
-                this.mostrar=true;
-                this.seleccionCorrecta=true;
-                this.arreglo=[this.pokemonCorrecto];
-                this.mostrarError=false; // Ocultar mensaje de error si la selección es correcta
-            }else{
-                console.log('Errrororororor');
-                this.mostrarError=true; // Mostrar mensaje de error si la selección es incorrecta
-            }
+    // Incrementar el contador de intentos en cada selección
+    this.intento++;
+
+    if(dato.ident===this.pokemonCorrecto.id){
+        this.mostrar=true;
+        this.seleccionCorrecta=true;
+        this.arreglo=[this.pokemonCorrecto];
+        this.mostrarError=false; // Ocultar mensaje de error si la selección es correcta
+
+        // Actualizar la puntuación basada en el número de intentos
+        switch(this.intento) {
+            case 1:
+                this.puntuacion = 10;
+                break;
+            case 2:
+                this.puntuacion = 8;
+                break;
+            case 3:
+                this.puntuacion = 5;
+                break;
+            case 4:
+                this.puntuacion = 3;
+                break;
+            case 5:
+                this.puntuacion = 2;
+                break;
+            case 6:
+                this.puntuacion = 1;
+                break;
+            default:
+                this.puntuacion = 0;
         }
+    }else{
+        console.log('Errrororororor');
+        this.mostrarError=true; // Mostrar mensaje de error si la selección es incorrecta
+    }
+}
     },
     data () {
         return {
@@ -51,11 +80,15 @@ export default {
             pokemonCorrecto:null,
             mostrar:false,
             seleccionCorrecta:false,
-            mostrarError:false // Nueva propiedad para controlar el mensaje de error
+            mostrarError:false, 
+            puntuacion :0,
+            intento: 0,
+       
+       
         };
     },
     mounted(){
-        this.cargaInicial(4);
+        this.cargaInicial(7);
     }
 };
 </script>
